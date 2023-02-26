b19fe98e3f15331cca1ddbe98a819255cd703e4a
Isaac Ongera <isaacongera717@gmail.com>
2023-02-26 15:23:12 +0300

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperature TEXT,
+    alive INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr.Chocolate', 50, 'M', 'blue', 'happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Rowdy', 14, 'M', 'red', 'rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Tabitha', 35, 'F', 'grey', 'salty', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 35, 'M', 'brown', 'somber', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Melissa', 17, 'F', 'purple', 'sad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Grinch', 28, 'M', 'green', 'mad', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Wendy', 22, 'F', 'pink', 'happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 15, 'M', 'blue', 'sad', 1);
